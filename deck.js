6d8f25e4c2eac56011cc4fa63b16ff7ece9abbe0
kiralybalint <kiraly.balint@simonyi.bme.hu>
2020-02-14 21:40:13 +0100

    Update mdx-deck and add custom theme

--- deck.js
+++
@@ -1,6 +0,0 @@
-import { slides as css } from './slides/css/index.mdx';
-import { slides as html } from './slides/html/index.mdx';
-import { slides as introduction } from './slides/introduction/index.mdx';
-import { slides as webBasic } from './slides/web-basic/index.mdx';
-
-export const slides = [...introduction, ...webBasic, ...html, ...css];
---
+++ theme.js
@@ -0,0 +1,23 @@
+// Example theme.js
+export default {
+    fonts: {
+      body: 'Roboto, sans-serif',
+      monospace: '"Roboto Mono", monospace',
+      size: "10px",
+    },
+    styles: {
+
+      Slide: {
+        fontSize: [6],
+        pre: {
+          fontSize: [3],
+          minWidth: "50%",
+        },
+      },
+    },
+    colors: {
+      text: 'black',
+      background: 'white',
+      primary: 'blue',
+    },
+  }
